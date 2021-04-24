b4d94f1b21676ee6d7fb1e0cab00051e6824face
Mayuri-Shatkunarajah-11 <81631368+Mayuri-Shatkunarajah-11@users.noreply.github.com>
2021-04-24 18:12:07 +0200

    I have done some changes

---
+++ Day10/javascript/script1.js
@@ -0,0 +1,14 @@
+function myFunction(){
+
+let partnerName="Shatkunarajah";
+let numOfChildren=1;
+let place="argau";
+let jobTitle="Software programmer";
+
+
+console.log(`You will be a ${jobTitle} in ${place},and married to ${partnerName} with ${numOfChildren}kid`);
+/*message="You will be a"+""+jobTitle+"in"+location+"married to"+partnerName+"with"+numOfChildren;
+console.log(message);*/
+
+}
+myFunction();
